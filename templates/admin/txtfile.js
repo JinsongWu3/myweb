3f630a7a3919f7dece88520bf1f1e893ae6e5f7f
wujs <269926183@qq.com>
2020-03-04 14:02:06 +0800

    init

---
+++ templates/admin/txtfile.js
@@ -0,0 +1,20 @@
+function submit_txt(form, url_name) {
+    document.getElementById("crop").removeAttribute("disabled");
+    $.ajax({
+        //几个参数需要注意一下
+        type: "POST",//方法类型
+        dataType: "json",//预期服务器返回的数据类型
+        url: url_name,//url
+        data: $("#" + form).serialize(),
+        success: function (result) {
+            console.log(result);//打印服务端返回的数据(调试用)
+            if (result.resultCode == 200) {
+                alert("SUCCESS");
+            }
+            ;
+        },
+        error: function () {
+            alert("异常！");
+        }
+    });
+}
