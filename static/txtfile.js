2f2b2296f35434712ecd2a703dcf5b308078bd9e
wujs <269926183@qq.com>
2020-03-05 10:05:13 +0800

    init

--- static/txtfile.js
+++ static/txtfile.js
@@ -1,5 +1,8 @@
 function submit_txt(form, url_name) {
+    $("#preview").removeAttr("disabled");
     $.ajax({
+
+
         //几个参数需要注意一下
         type: "POST",//方法类型
         dataType: "json",//预期服务器返回的数据类型
@@ -8,7 +11,9 @@ function submit_txt(form, url_name) {
         success: function (result) {
             console.log(result);//打印服务端返回的数据(调试用)
             if (result.resultCode == 200) {
-                alert("SUCCESS");
+		$("#preview").removeAttr("disabled");
+		alert("SUCC_ESS");
+
             }
             ;
         },
